45eecc1711d8bbb129e0a90a06f49641432211e9
gopes <gopes@LAPTOP-KOAKE35K>
2022-07-15 15:00:39 +0530

    commiting

---
+++ com.crm.vtigermaven.SDET36/src/test/java/com/crm/vtigermaven/SDET36test/Address_test.java
@@ -0,0 +1,18 @@
+package com.crm.vtigermaven.SDET36test;
+
+import org.testng.annotations.Test;
+
+public class Address_test {
+	
+@Test(groups = "smoke")
+public void name()
+{
+System.out.println("hii");	
+}
+@Test(groups = "smoke")
+public void address()
+{
+	System.out.println("bye");
+}
+
+}
---
+++ com.crm.vtigermaven.SDET36/src/test/java/com/crm/vtigermaven/SDET36test/Name_test.java
@@ -0,0 +1,19 @@
+package com.crm.vtigermaven.SDET36test;
+
+import org.testng.annotations.Test;
+
+public class Name_test {
+	
+@Test(groups = "regression")
+public void name2()
+{
+System.out.println("shiv");	
+}
+
+@Test(groups = "smoke")
+public void address()
+{
+System.out.println("home");	
+}
+
+}
